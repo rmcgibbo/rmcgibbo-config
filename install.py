51298776dc24083faea770e1147432c519c903e5
Robert McGibbon <rmcgibbo@gmail.com>
2016-03-15 10:01:44 -0400

    Futurize install.py

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import os
 import sys
 import shutil
@@ -17,21 +18,21 @@ def link(src, dest):
     if os.path.exists(dest):
         if os.path.islink(dest):
             if os.path.abspath(os.readlink(dest)) == src:
-                print 'Link already in place: %s' % src
+                print('Link already in place: %s' % src)
                 return
         
         backup = dest + '.backup.' + strftime("%Y.%m.%d")
-        print 'Backing up current copy to %s' % backup
+        print('Backing up current copy to %s' % backup)
         if os.path.exists(backup): raise Exception('Backup already exists')
         os.rename(dest, backup)
     
-    print 'Softlinking %s to %s' % (src, dest)
+    print('Softlinking %s to %s' % (src, dest))
     os.symlink(src, dest)
 
 
 def main():
-    print "Linking each config file in its correct place"
-    print '=-'*30
+    print("Linking each config file in its correct place")
+    print('=-'*30)
     
     # link the base .bashrc that the system specfic ones source
     link('_bashrc.base', '~/.bashrc.base')
@@ -41,7 +42,7 @@ def main():
         hostname_root = os.uname()[1].split('.')[0]
         link('_bashrc.' + hostname_root, '~/.bashrc')
     except Exception as e:
-        print 'Using default bashrc'
+        print('Using default bashrc')
         link('_bashrc.default', '~/.bashrc')
 
     # link _bash_profile to .bash_profile
@@ -63,7 +64,7 @@ def main():
     elif os.path.exists(os.path.expanduser('~/.config/ipython/profile_default')):
         link('ipython_config.py', '~/.config/ipython/profile_default/ipython_config.py')
     else:
-        print 'Could not find an ipython config directory'
+        print('Could not find an ipython config directory')
         
     # link ipython_config_profile_msm.py
     if os.path.exists(os.path.expanduser('~/.ipython/profile_msm')):
@@ -71,7 +72,7 @@ def main():
     elif os.path.exists(os.path.expanduser('~/.config/ipython/profile_msm')):
         link('ipython_config_profile_msm.py', '~/.config/ipython/profile_msm/ipython_config.py')
     else:
-        print 'Could not find an ipython config directory for profile=msm'
+        print('Could not find an ipython config directory for profile=msm')
         
     # link ipython_config_profile_msm.py
     if os.path.exists(os.path.expanduser('~/.ipython/profile_units')):
@@ -79,7 +80,7 @@ def main():
     elif os.path.exists(os.path.expanduser('~/.config/ipython/profile_units')):
         link('ipython_config_profile_units.py', '~/.config/ipython/profile_units/ipython_config.py')
     else:
-        print 'Could not find an ipython config directory for profile=units'
+        print('Could not find an ipython config directory for profile=units')
 
     # link matplotlibrc
     if sys.platform.startswith('linux'):
@@ -93,12 +94,12 @@ def main():
     if 'TEXINPUTS' in os.environ:
         link('rmcgibbo-latex.sty', '~/.LaTeX/rmcgibbo-latex.sty')
     else:
-        print 'Skipping installing custom LaTeX style file because'
-        print "I didn't find the 'TEXINPUT' environment variable"
-        print "(It should point to ~/.LaTeX for this to work)"
-        print "After creating ~/.LaTeX, you will probably need to run"
-        print "the command 'texhash' to rebuild the LaTeX hash to find"
-        print "the new packages."
+        print('Skipping installing custom LaTeX style file because')
+        print("I didn't find the 'TEXINPUT' environment variable")
+        print("(It should point to ~/.LaTeX for this to work)")
+        print("After creating ~/.LaTeX, you will probably need to run")
+        print("the command 'texhash' to rebuild the LaTeX hash to find")
+        print("the new packages.")
 
 if __name__ == '__main__':
     main()
